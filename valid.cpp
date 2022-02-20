05b4e6bdc6c7b1a75116b8c9f149b2d1dabe4bad
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-02-19 19:52:33 -0500

    Update valid.cpp

--- valid.cpp
+++ valid.cpp
@@ -11,9 +11,11 @@ using namespace std;
 int main()
 {
     int i;
+    //input will be integer "i"
     cout << "Input a number: ";
     cin >> i;
     if(0 < i && i < 100){
+    //sets condition to be 1-99
         cout << i * i << endl;
     }
     else{
