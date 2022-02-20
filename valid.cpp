acd66b32c96f6cb03df139462e6751b9364cc497
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-02-19 19:51:30 -0500

    Create valid.cpp

---
+++ valid.cpp
@@ -0,0 +1,25 @@
+/*
+Author: Sakib Khan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 2 A
+This program will square a number if between the range of 1-99. It will input "invalid" otherwise.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout << "Input a number: ";
+    cin >> i;
+    if(0 < i && i < 100){
+        cout << i * i << endl;
+    }
+    else{
+        cout << "Invalid";
+    }
+
+    return 0;
+
+}
